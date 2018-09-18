b10adb22bcd1b4dd2fd189de94ff8dc49f81cec1
Antonello Fazio <antonello.fazio@gmail.com>
2018-09-18 09:56:09 +0200

    Convert to MVC design pattern

---
+++ Todoey/Controllers/TodoListViewController.swift
@@ -0,0 +1,143 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Antonello Fazio on 15/9/18.
+//  Copyright © 2018 Malcom Logan. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    @IBOutlet var toDoTableView: UITableView!
+
+    
+    var itemArray : Array = [Item]()
+    
+    let defaults = UserDefaults.standard
+    
+    
+    //MARK: - viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureTableView()
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
+            itemArray = items
+        }
+        
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destroy Dermogorgon"
+        itemArray.append(newItem3)
+    
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        itemArray.append(newItem3)
+        
+    }
+
+
+    //MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath) as! ToDoTableViewCell
+        
+        let item = itemArray[indexPath.row]
+        cell.toDoLabel.text = item.title
+        
+         cell.accessoryType = item.done ? .checkmark : .none // il codice accanto (Ternary operator) esprime in una riga quello che c'è commentato qui sotto
+        
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    
+    //MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    //MARK: - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Todoey item", message: "", preferredStyle: .alert)
+        let addItemAction = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            if textField.text == "" {
+                
+            } else {
+                
+                let itemAdded = Item()
+                itemAdded.title = textField.text!
+                self.itemArray.append(itemAdded)
+                self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+                self.tableView.reloadData()
+            }
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(addItemAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func configureTableView() {
+        toDoTableView.rowHeight = UITableViewAutomaticDimension
+        toDoTableView.estimatedRowHeight = 120
+    }
+}
+
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Antonello Fazio on 18/9/18.
+//  Copyright © 2018 Malcom Logan. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
